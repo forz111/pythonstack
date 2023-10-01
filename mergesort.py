0df520d4995cf1118b3d6ace6527a103a3979dae
forz <forzes0401@gmail.com>
2023-10-02 01:20:27 +0200

    132

--- mergesort.py
+++ mergesort.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr
@@ -47,18 +46,4 @@ def merge(left, right):
 arr = [int(x) for x in input().split()]
 sorted_arr = merge_sort(arr)
 print(sorted_arr)
-=======
-import bisect
 
-n = int(input())
-line = list(map(int, input().split()))
-
-l2 = [line[-1]]
-answer = 0
-for elem in reversed(line[:-1]):
-    new_ind = bisect.bisect_left(l2, elem)
-    answer += new_ind
-    l2.insert(new_ind, elem)
-
-print(answer)
->>>>>>> origin/master
--- quicksort.py
+++ quicksort.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def quicksort(arr):
     if len(arr) <= 1:
         return arr
@@ -15,50 +14,4 @@ def quicksort(arr):
 arr = [int(i) for i in input().split()]
 sorted_arr = quicksort(arr)
 print(sorted_arr)
-=======
-from random import randint
 
-
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    x = arr[randint(0, len(arr) - 1)]
-    left = [i for i in arr if i < x]
-    mid = [i for i in arr if i == x]
-    right = [i for i in arr if i > x]
-    return quick_sort(left) + mid + quick_sort(right)
-
-
-def dot_in_a(d, arr):
-    left, right = 0, len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] > d:
-            right = mid - 1
-        elif arr[mid] <= d:
-            left = mid + 1
-    return right + 1
-
-
-def dot_in_b(d, arr):
-    left, right = 0, len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] >= d:
-            right = mid - 1
-        elif arr[mid] < d:
-            left = mid + 1
-    return right + 1
-
-
-n, m = map(int, input().split())
-ab = [list(map(int, input().split())) for i in range(n)]
-dots = list(map(int, input().split()))
-
-
-a = quick_sort([i[0] for i in ab])
-b = quick_sort([i[1] for i in ab])
-
-for i in dots:
-    print(dot_in_a(i, a) - dot_in_b(i, b), end=' ')
->>>>>>> origin/master
