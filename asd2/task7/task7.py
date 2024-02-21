743f2b0a2d7d76ff95da8599191d77a72cd6598d
forz <forzes0401@gmail.com>
2024-02-21 14:35:21 +0200

    Updated alg

--- asd2/task7/task7.py
+++ asd2/task7/task7.py
@@ -1,5 +1,5 @@
-import sys
 import heapq
+
 
 def prim(graph, start_node):
     mst = []
@@ -15,19 +15,22 @@ def prim(graph, start_node):
                     heapq.heappush(edges, (edge_weight, edge))
     return mst
 
+
 def read_graph_from_file(filename):
     with open(filename, 'r') as file:
         graph = [[int(num) for num in line.split()] for line in file]
     return graph
 
+
 def write_result_to_file(filename, result):
     with open(filename, 'w') as file:
         file.write(' '.join(map(str, result)))
+
 
 def main():
     graph = read_graph_from_file('graph.txt')
     mst = prim(graph, 0)
     write_result_to_file('output.txt', mst)
 
-if __name__ == "__main__":
-    main()
+
+main()
