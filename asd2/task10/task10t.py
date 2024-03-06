5e3ba4f1a9269ffeba98337db0af8cf81f40dcaa
forz <forzes0401@gmail.com>
2024-03-06 10:51:04 +0200

    Updated lab10

--- asd2/task10/task10t.py
+++ asd2/task10/task10t.py
@@ -1,65 +1,78 @@
+def is_connected(graph):
+    # Начинаем с вершины 0
+    start_node = 0
+    # Создаем список для отслеживания посещенных вершин
+    color = [0] * len(graph)
+    # Помечаем начальную вершину как посещенную
+    color[start_node] = 1
+    # Создаем стек и добавляем в него начальную вершину
+    stack = [start_node]
+    # Пока стек не пуст
+    while stack:
+        # Берем вершину из стека
+        node = stack[-1]
+        stack.pop()
+        # Перебираем все вершины графа
+        for i in range(len(graph)):
+            # Если есть ребро и вершина еще не была посещена
+            if graph[node][i] == 1 and color[i] == 0:
+                # Добавляем вершину в стек и помечаем ее как посещенную
+                stack.append(i)
+                color[i] = 1
+    # Если все вершины были посещены, то граф связный
+    return 0 not in color
+
+def is_eulerian(graph):
+    # Подсчитываем количество вершин с нечетной степенью
+    odd = 0
+    for row in graph:
+        degree = sum(row)
+        if degree % 2 == 1:
+            odd += 1
+    # Граф эйлеров, если нет вершин с нечетной степенью
+    return odd == 0
+
 def find_euler_tour(graph):
-    # Создаем пустой список для хранения тура
-    tour = []
-
-    # Создаем список ребер графа
-    # Для каждой вершины u в графе мы проходим по всем вершинам v
-    # Если между вершинами u и v есть ребро (graph[u][v] == 1), то добавляем пару (u, v) в список ребер E
-    E = [(u, v) for u in range(len(graph)) for v in range(len(graph)) if graph[u][v]]
-
-    # Создаем список степеней вершин графа
-    # Для каждой вершины u в графе мы проходим по всем вершинам v
-    # Если между вершинами u и v есть ребро (graph[u][v] == 1), то увеличиваем степень вершины u на 1
-    numEdges = [sum(1 for v in range(len(graph)) if graph[u][v]) for u in range(len(graph))]
-
-    # Проверяем, что все вершины имеют четную степень и список ребер не пуст
-    # Если это так, то начинаем поиск тура с первой вершины
-    # В противном случае возвращаем None, так как Эйлеров цикл не существует
-    if all(num % 2 == 0 for num in numEdges) and E:
-        find_tour(E[0][0], E, tour, numEdges)
-        tour.reverse()
-        return tour
-    else:
+    # Проверяем, является ли граф связным и эйлеровым
+    if not is_connected(graph) or not is_eulerian(graph):
         return None
-
-def find_tour(u, E, tour, numEdges):
-    # Проходим по всем ребрам графа
-    for (a, b) in E:
-        # Если текущая вершина совпадает с одним из концов ребра
-        if a == u:
-            # Удаляем ребро из списка ребер
-            E.remove((a, b))
-            # Уменьшаем степень соответствующих вершин
-            numEdges[a] -= 1
-            numEdges[b] -= 1
-            # Продолжаем поиск тура с другого конца ребра
-            find_tour(b, E, tour, numEdges)
-        # Если текущая вершина совпадает с другим концом ребра
-        elif b == u:
-            # Удаляем ребро из списка ребер
-            E.remove((a, b))
-            # Уменьшаем степень соответствующих вершин
-            numEdges[a] -= 1
-            numEdges[b] -= 1
-            # Продолжаем поиск тура с другого конца ребра
-            find_tour(a, E, tour, numEdges)
-    # Добавляем текущую вершину в тур
-    tour.append(u)
+    # Находим вершину с нечетной степенью или, если такой нет, начинаем с вершины 0
+    u = next((i for i, v in enumerate(graph) if sum(v) % 2 == 1), 0)
+    # Создаем стек и путь
+    stack = [u]
+    path = []
+    # Пока стек не пуст
+    while stack:
+        # Берем вершину из стека
+        u = stack[-1]
+        # Если у вершины есть соседи
+        if any(graph[u]):
+            # Находим первого соседа
+            v = graph[u].index(1)
+            # Удаляем ребро между u и v
+            graph[u][v] = graph[v][u] = 0
+            # Добавляем вершину в стек
+            stack.append(v)
+        else:
+            # Если у вершины нет соседей, добавляем ее в путь и удаляем из стека
+            path.append(stack.pop())
+    # Возвращаем перевернутый путь
+    return path[::-1]
 
 def main():
     # Открываем файл для чтения
-    with open('graph2.txt', 'r', encoding='utf-8') as f:
+    with open('graph.txt', 'r', encoding='utf-8') as f:
         # Читаем матрицу смежности из файла
         graph = [list(map(int, line.split())) for line in f]
     # Находим Эйлеров тур
     tour = find_euler_tour(graph)
     # Открываем файл для записи
-    with open('output4.txt', 'w', encoding='utf-8') as f:
+    with open('output3.txt', 'w', encoding='utf-8') as f:
         # Если Эйлеров тур существует, то записываем его в файл
         # В противном случае записываем сообщение о том, что Эйлеров цикл не существует
         if tour is not None:
             for node in tour:
-                f.write(str(node) + '\n')
+                f.write(f"->{str(node)}")
         else:
             f.write("Эйлеров цикл не существует в данном графе.\n")
 
